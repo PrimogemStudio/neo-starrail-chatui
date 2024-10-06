4cc0b3f7057ab8abe3b60267466dbc69c4d492c5
Jack253-png <guoxiuchen20170402@163.com>
2024-10-06 18:03:07 +0800

    Update

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:neo_starrail_chatui/packs/starrail_button.dart';
+import 'package:neo_starrail_chatui/packs/starrail_list.dart';
 import 'package:neo_starrail_chatui/packs/starrail_page.dart';
+import 'package:neo_starrail_chatui/packs/starrail_panel.dart';
 
 class ChatPage extends StatefulWidget implements NamedPage {
   const ChatPage({super.key});
@@ -20,10 +22,22 @@ class ChatPage extends StatefulWidget implements NamedPage {
 }
 
 class ChatPageState extends State<ChatPage> {
+  GlobalKey<StarRailListState> listKey = GlobalKey();
+  GlobalKey<StarRailPanelState> panelState = GlobalKey();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: TextButton(onPressed: () {
-      Navigator.of(context).pushReplacementNamed("/channels");
-    }, style: srStyle, child: const Text("data")));
+    return Scaffold(body: StarRailList(key: listKey, innerPanel: StarRailPanel(key: panelState, func: () {}, onMoving: () { listKey.currentState!.scrollToBottomImm(); }), flatted: false),
+        floatingActionButton: Column(
+            verticalDirection: VerticalDirection.up,
+            children: [
+              FloatingActionButton(onPressed: () {
+                Navigator.of(context).pushReplacementNamed("/channels");
+              }),
+              FloatingActionButton(onPressed: () {
+                panelState.currentState!.openPanel();
+              })
+            ]
+        )
+    );
   }
 }
