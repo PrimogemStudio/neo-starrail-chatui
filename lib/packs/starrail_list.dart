538ae72c3fe150ddb1d5d94fa5499f3598af6af4
Jack253-png <guoxiuchen20170402@163.com>
2024-10-19 16:16:29 +0800

    Remove

--- lib/packs/starrail_list.dart
+++ lib/packs/starrail_list.dart
@@ -28,20 +28,9 @@ class StarRailList extends StatefulWidget {
 }
 
 class StarRailListState extends State<StarRailList> {
-  Offset? dragOff;
-  double currOff = 0;
-  bool dragging = false;
-  bool draggingInner = false;
-  double targetOff = 0;
-
   final ScrollController _controller = ScrollController();
   final GlobalKey<AnimatedListState> key = GlobalKey<AnimatedListState>();
   final GlobalKey barKey = GlobalKey();
-
-  double _offset = 0;
-  double _height = 0;
-  double _schHeight = 0;
-  double _po = 1;
 
   int currentOffset = 0;
 
@@ -107,45 +96,9 @@ class StarRailListState extends State<StarRailList> {
     _controller.jumpTo(_controller.position.maxScrollExtent);
   }
 
-  void onRecalc() {
-    if (_controller.positions.isEmpty) return;
-    var height = key.currentContext!.size!.height;
-    var maxScrollExtent = _controller.position.maxScrollExtent;
-    var offset = _controller.offset;
-
-    _height = height - 30;
-    _po = (height + maxScrollExtent) /
-        height /
-        _height *
-        height;
-    _schHeight =
-        _height * (height / (height + maxScrollExtent));
-    _offset = (_height - _schHeight) * (offset / maxScrollExtent);
-    if (_offset.isNaN) {
-      _offset = 0;
-    }
-    if (dragging) {
-      _controller.jumpTo(targetOff);
-    }
-  }
-
   @override
   void initState() {
     super.initState();
-
-    dynamic c;
-    c = (t) {
-      SchedulerBinding.instance.addPostFrameCallback((Duration d) {
-        if (mounted) {
-          setState(() {
-            onRecalc();
-          });
-        }
-      });
-
-      WidgetsBinding.instance.addPostFrameCallback(c);
-    };
-    WidgetsBinding.instance.addPostFrameCallback(c);
 
     _controller.addListener(() {
       final scrollDirection = _controller.position.userScrollDirection;
@@ -154,7 +107,6 @@ class StarRailListState extends State<StarRailList> {
             (scrollDirection == ScrollDirection.reverse ? -50 : 50);
         if (_controller.offset == _controller.position.minScrollExtent ||
             _controller.offset == _controller.position.maxScrollExtent) return;
-        dragging = false;
         _controller.jumpTo(scrollEnd);
       }
     });
@@ -180,101 +132,10 @@ class StarRailListState extends State<StarRailList> {
       physics: const BouncingScrollPhysics(),
     );
 
-    final innerBar = Listener(
-        child: RoundedRect(
-            width: 4,
-            height: _schHeight,
-            radius: 0,
-            color: widget.flatted ? Colors.transparent : uiViewBarMain),
-        onPointerMove: (e) {
-          targetOff = (e.localPosition.dy - dragOff!.dy) * _po + currOff;
-        },
-        onPointerDown: (e) {
-          dragOff = e.localPosition;
-          currOff = _controller.offset;
-          targetOff = currOff;
-          dragging = e.buttons == 1;
-          draggingInner = true;
-        },
-        onPointerUp: (e) {
-          dragging = false;
-          draggingInner = false;
-        });
-    final barBg = RoundedRect(
-        key: barKey,
-        width: 4,
-        height: _height,
-        radius: 0,
-        color: widget.flatted ? Colors.transparent : uiViewBarBg
-    );
-
-    final mainBar = Padding(
-        padding: const EdgeInsets.only(
-            left: 10, right: 10, top: 10, bottom: 20),
-        child: Listener(
-          child: Column(children: [
-            Stack(
-              alignment: Alignment.topRight,
-              children: [
-                barBg,
-                Positioned(top: _offset, child: innerBar)
-              ],
-            )
-          ]),
-          onPointerMove: (e) {
-            targetOff =
-                (e.localPosition.dy - dragOff!.dy) * _po + currOff;
-          },
-          onPointerUp: (e) {
-            dragging = false;
-          },
-          onPointerDown: (e) {
-            if (!draggingInner) {
-              _controller.jumpTo(e.localPosition.dy /
-                  _height *
-                  (_height - _schHeight) *
-                  _po);
-            }
-            dragOff = e.localPosition;
-            currOff = _controller.offset;
-            targetOff = currOff;
-            dragging = e.buttons == 1;
-            dragging = true;
-          },
-        ));
-
     final viewpanel = ScrollConfiguration(
         behavior:
         const ScrollBehavior().copyWith(scrollbars: false),
-        child: Listener(
-            child: widget.view!,
-            onPointerMove: (e) {
-              targetOff =
-                  (dragOff!.dy - e.localPosition.dy) + currOff;
-            },
-            onPointerDown: (e) {
-              dragOff = e.localPosition;
-              currOff = _controller.offset;
-              targetOff = currOff;
-              dragging = e.buttons == 1;
-            },
-            onPointerUp: (e) {
-              dragging = false;
-            }));
-    var b = Container(
-      height: 8,
-      decoration: BoxDecoration(
-        shape: BoxShape.rectangle,
-        gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: [
-              uiSurfaceColorTrans,
-              uiSurfaceColor
-            ]
-        ),
-      ),
-    );
+        child: widget.view!);
 
     return Stack(
       alignment: Alignment.topRight,
@@ -285,7 +146,6 @@ class StarRailListState extends State<StarRailList> {
             widget.innerPanel?? Container(),
           ]
         ),
-        mainBar,
         Column(children: [
           Container(
             height: 1,
