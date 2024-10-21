aebac1859c5d8332b117f647bee2eaa4bf696e23
Jack253-png <guoxiuchen20170402@163.com>
2024-10-21 10:45:13 +0800

    更改主题

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:neo_starrail_chatui/controls/sr_header.dart';
 import 'package:neo_starrail_chatui/controls/sr_list.dart';
 import 'package:neo_starrail_chatui/controls/sr_userbutton.dart';
 
+import 'controls/sr_colors.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -15,8 +17,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+          colorScheme: ColorScheme.light(surface: uiSurfaceColor),
+          useMaterial3: true,
         fontFamily: "StarRailFont_bundle",
         fontFamilyFallback: const ["u2400"]
       ),
@@ -40,10 +42,13 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title: SrHeader(key: headerKey, replyer: "", replyerDesc: null),
-        ),
+        appBar: AppBar(
+            backgroundColor: uiSurfaceColor,
+            shadowColor: Colors.transparent,
+            elevation: 0,
+            title: SrHeader(key: headerKey, replyer: "", replyerDesc: ""),
+            scrolledUnderElevation: 0,
+            surfaceTintColor: Colors.transparent),
         body: SrList(key: listKey, invertDrag: true),
         floatingActionButton: Column(children: [
           FloatingActionButton(onPressed: () {
