ae44e84d0f696f75bf539baa1bc90014d7f34d5c
Jack253-png <guoxiuchen20170402@163.com>
2024-10-21 14:25:19 +0800

    添加登录页面内容

--- lib/page/login_page.dart
+++ lib/page/login_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../controls/sr_buttonstyles.dart';
+
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
 
@@ -8,8 +10,34 @@ class LoginPage extends StatefulWidget {
 }
 
 class LoginPageState extends State<LoginPage> {
+  var username = TextEditingController();
+  var password = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Text("test!${DateTime.timestamp()}"));
+    return Scaffold(
+        body: Center(
+            child: SizedBox(
+                width: MediaQuery.of(context).size.width / 2,
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      TextField(
+                          controller: username,
+                          decoration: const InputDecoration(
+                            hintText: '用户名',
+                          )),
+                      const Padding(padding: EdgeInsets.all(5)),
+                      TextField(
+                          controller: password,
+                          obscureText: true,
+                          decoration: const InputDecoration(
+                            hintText: '密码',
+                          )),
+                      const Padding(padding: EdgeInsets.all(5)),
+                      ElevatedButton(
+                          style: srStyle,
+                          child: const Text('登录'),
+                          onPressed: () async {})
+                    ]))));
   }
 }
